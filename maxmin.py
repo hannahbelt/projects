fcd4c2a66a1b0653c8787f3f3845e34a7962491a
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-6.local>
2018-09-09 23:24:32 -0400

    maxmin.py

---
+++ maxmin.py
@@ -0,0 +1,19 @@
+largest = None
+smallest = None
+print 'Awaiting input:', largest
+while True:
+  line = raw_input('Enter a number: ')
+  if line == 'done':
+    break
+  try:
+    if float(line):
+      itervar = float(line)
+      if largest is None or itervar > largest:
+        largest = itervar
+      print 'Largest_number:', largest
+      if smallest is None or itervar < smallest:
+        smallest = itervar
+      print 'smallest_number:', smallest
+  except:
+      print ('Error: input')
+
