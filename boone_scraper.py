f3988a2330d5bee1075646d9b05e69996261a690
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-09-18 17:55:21 -0400

    finished pulling data

--- boone_scraper.py
+++ boone_scraper.py
@@ -1,6 +1,7 @@
 #
 
 # python imports
+import csv
 # external imports
 import requests
 from bs4 import BeautifulSoup
@@ -16,7 +17,22 @@ html = response.content
 soup = BeautifulSoup(html, 'lxml')
 # print(soup.prettify())
 table = soup.find('tbody', attrs= {'class': 'stripe'})
-print(table)
+# print(table)
+
 # extract
+list_of_rows = []
+for row in table.findAll('tr'):
+    # print(row.prettify())
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        list_of_cells.append(cell.text)
+
+    #print(list_of_cells)
+   # print(list_of_cells[:-1])
+    list_of_rows.append(list_of_cells[:-1])
 
 # write
+outfile = open('data/inmates.csv', 'w+')
+writer = csv.writer(outfile)
+writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerows(list_of_rows)
