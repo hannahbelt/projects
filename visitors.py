325790d8f0c80d99ae82679e6584b55085b29533
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-09-23 15:19:13 -0400

    created csv

--- visitors.py
+++ visitors.py
@@ -30,27 +30,11 @@ for row in table.findAll('tr'):
         for cell in row.findAll('div'):
             # add it to the list of cells
             list_of_cells.append(cell.text)
-    #print(list_of_cells)
-    list_of_rows.append(list_of_cells)
+        #print(list_of_cells)
+        list_of_rows.append(list_of_cells)
 for row in list_of_rows:
     print(row)
 
-        # if this isn't empty
-        #if list_of_cells:
-            # make sure we get only the data we want:
-            # print(list_of_cells)
-            # eh, that last thing in the list is a little iffy, let's access it by index:
-            # print(list_of_cells[-1])
-            # clean off the whitespace gunk characters:
-            # print(list_of_cells[-1].strip())
-            # nah, what if we ignore it entirely:
-            # print(list_of_cells[:-1])
-
-            # add the list of cells, each list representing a row, to the list of rows -- except the last cell. screw that dude.
-        #list_of_rows.append(list_of_cells[:-1])
-
-    # give back the list of rows to whatever calls this function
-    #return list_of_rows
 
 
 # write
@@ -62,18 +46,3 @@ writer.writerow(['Park', 'Rank', 'Recreation Visits', 'Percent of Total'])
 writer.writerows(list_of_rows)
 
 
-# do everything all together
-#def scrape_jail_records(url, filename, column_names):
-#content = get_page_content(url)
-#formatted_html = parse_html(content)
-#data = extract_data(formatted_html)
-#write_file(data, column_names, filename)
-
-
-#if __name__ == '__main__':
-   # url = 'https://irma.nps.gov/Stats/Reserved.ReportViewerWebControl.axd?OpType=Resource&Version=11.0.2802.16&Name=ViewerScript'
-    #filename = 'data/visitors.csv'
-    #column_names = [
-       # 'Park', 'Rank', 'Recreation Visits', 'Percent of Total'
-   # ]
-    #scrape_jail_records(url, filename, column_names)
