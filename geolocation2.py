fc11bdd0371dc8642557eb53d21c539ac500fc31
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-10-07 21:40:30 -0400

    added comments

--- geolocation2.py
+++ geolocation2.py
@@ -1,38 +1,35 @@
 #!usr/local/bin/env python3
 
 # external imports
+from pprint import pprint
 import requests
 # project-level imports
 from secret_settings import geojson_stub, geojson_key
 
 
-def ping_api(address):
+# Gets the address from the API from Google
+def pull_api(address):
     p = {'address': address, 'key': geojson_key}
-    # print(requests.get(geojson_stub, params=p))
     return requests.get(geojson_stub, params=p)
 
 
+# creates function to 
 def get_geolocation_data(address):
         county = ''
         state_long = ''
         state_short = ''
         country = ''
 
-        response = ping_api(address)
+        response = pull_api(address)
         if response.status_code == 200:
             # print(response.json())
 
             # addresses are self-reported and human-entered; they will always be
             # a little messy and the API will always miss a few
             try:
-                # print(response.json()['results'])
-                # print(response.json()['results'][0])
-                # print(response.json()['results'][0]['address_components'])
+                #['address_components'])
                 address_components = response.json()['results'][0]['address_components']
 
-                # the API is maddeningly stingy with lookups -- especially for data
-                # that can move index positions. I feel dirty writing a loop here.
-                # But Google made me.
                 for line in address_components:
                     if line['types'][0] == 'administrative_area_level_2':
                         county = line['long_name']
@@ -42,8 +39,10 @@ def get_geolocation_data(address):
                     elif line['types'][0] == 'country':
                         country = line['long_name']
 
+               # organizes the list within the API stuff
                 geolocator = {}
 
+                # narrows down the list to only be what we need
                 geolocator['clean_address'] = response.json()['results'][0]['formatted_address']
                 geolocator['latitude'] = response.json()['results'][0]['geometry']['location']['lat']
                 geolocator['longitude'] = response.json()['results'][0]['geometry']['location']['lng']
@@ -58,12 +57,12 @@ def get_geolocation_data(address):
             except IndexError:
                 return None
 
-
+# creates the sentence format about the address
 def write_output_sentence(geolocator):
     # print(geolocator)
-    print(f'My favorite place is my own home. My address is {geolocator["clean_address"][:-5]}, at latitude {geolocator["latitude"]} and longitude {geolocator["longitude"]}, in {geolocator["county"]}, {geolocator["state_long"]}, {geolocator["country"]}.')
+    print(f'The address for the Media Innovation Center is {geolocator["clean_address"][:-5]}, at latitude {geolocator["latitude"]} and longitude {geolocator["longitude"]}, in {geolocator["county"]}, {geolocator["state_long"]}, {geolocator["country"]}.')
 
-
+# gets the data to put in the sentence 
 def geolocate(address):
     geolocator_data = get_geolocation_data(address)
     if geolocator_data:
@@ -71,7 +70,7 @@ def geolocate(address):
     else:
         print(geolocator_data)
 
-
+# allows you to use code for any address
 if __name__ == '__main__':
     address = '4 Ore Mill Pl., Thurmont, MD'
     # address = '4 Ore Mill Pl., Thurmont, MD'
