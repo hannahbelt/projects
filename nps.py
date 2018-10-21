b8007fc0d24297eed476b737922459ca8ea50ed0
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-10-21 01:47:26 -0400

    url and outline

---
+++ nps.py
@@ -0,0 +1,36 @@
+# python imports
+import json
+# external imports
+import requests
+from bs4 import BeautifulSoup
+
+
+
+#ids
+# need to figure out how to get ALL the ids
+nat_park_ids = [
+    
+]
+
+#url components
+url_stub = 'https://irma.nps.gov'
+
+nps_uri = '/Stats/MvcReportViewer.aspx?_id=6d4c57ec-773a-4be8-a7d7-4410daaa91ad&_m=Remote&_r=%2fNPS.Stats.Reports%2fPark+Specific+Reports%2fMonthly+Visitation+Comments+By+Park&_39=880px&Park='
+
+# build loop for comments
+# skip over comments that dont have errors in them
+# common "errors" are recorded
+
+# get page content
+
+#parse html
+
+#extract data
+
+# build dictionary for comments
+
+# write
+
+#call all functions
+
+# if name = main stuff here?
