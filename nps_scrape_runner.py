9c4dd182a09beedc581a3c66ed200a93f4389309
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-12-07 18:30:08 -0500

    loop started

--- nps_scrape_runner.py
+++ nps_scrape_runner.py
@@ -11,3 +11,20 @@ with open('all_park_visitors', 'r') as read_file_visitors:
 # google merge two dictionaries shared keys python
 
 comments_and_visitors = {}
+for key in visitors
+    if key is not None:
+        # add to full dict for ID to new dict
+
+#  Loop through the visitors' keys
+
+# -- For each key in visitors, check the comments dictionary to see if there are any comments for that park id
+
+# -- If there aren't, skip it and keep going. Nothing to do
+
+# -- If there are, add the full dictionary for that park id to the new dictionary we're creating
+
+# -- Slice off the year from the comment date to determine which year's visitors data is relevant  -- so depending on the structure you have, you may need to loop through those comments to check each date
+
+# -- In the new dictionary we're creating, add a 'comments' key to that year's data -- so keys will be like 'January', 'February', [... snip ...] 'Total', and 'comments' -- with a value of an empty dictionary
+
+# -- Set keys for that 'comments' dictionary to another dictionary with keys of 'date' and 'comment'
