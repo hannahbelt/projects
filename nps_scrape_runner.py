c91ae916370487c01ff72a9adb184b15d00a161e
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-12-09 17:56:15 -0500

    open jsons

--- nps_scrape_runner.py
+++ nps_scrape_runner.py
@@ -2,10 +2,10 @@
 # open other call something else and read it as json 
 # treat as dixtorionaries
 import json
-with open('all_park_comments', 'r') as read_file_comments:
+with open('data/all_park_comments.json', 'r') as read_file_comments:
     comments = json.load(read_file_comments)
 
-with open('all_park_visitors', 'r') as read_file_visitors:
+with open('data/all_park_visitors.json', 'r') as read_file_visitors:
     visitors = json.load(read_file_visitors)
 
 # google merge two dictionaries shared keys python
@@ -29,17 +29,3 @@ with open('data/comments_and_visitors.json', 'w+') as f:
     print(j, file=f)    
     
         # add to full dict for ID to new dict
-
-#  Loop through the visitors' keys
-
-# -- For each key in visitors, check the comments dictionary to see if there are any comments for that park id
-
-# -- If there aren't, skip it and keep going. Nothing to do
-
-# -- If there are, add the full dictionary for that park id to the new dictionary we're creating
-
-# -- Slice off the year from the comment date to determine which year's visitors data is relevant  -- so depending on the structure you have, you may need to loop through those comments to check each date
-
-# -- In the new dictionary we're creating, add a 'comments' key to that year's data -- so keys will be like 'January', 'February', [... snip ...] 'Total', and 'comments' -- with a value of an empty dictionary
-
-# -- Set keys for that 'comments' dictionary to another dictionary with keys of 'date' and 'comment'
