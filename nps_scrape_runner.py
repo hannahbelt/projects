737e125d00119a709bb57edab78c0d4f6265fa89
Hannah Belt <hannahbelt@Hannahs-MacBook-Air-7.local>
2018-12-11 12:10:41 -0500

    final

--- nps_scrape_runner.py
+++ nps_scrape_runner.py
@@ -1,6 +1,8 @@
 # open one file and read it as json
 # open other call something else and read it as json 
 # treat as dixtorionaries
+
+from pprint import pprint
 import json
 with open('data/all_park_comments.json', 'r') as read_file_comments:
     comments = json.load(read_file_comments)
@@ -25,11 +27,16 @@ for key in visitors.keys():
         comments_and_visitors[key] = park_visitors
         for comment in park_comments['park_comments']:
             comment_year = comment['date'][-4:]
-            #print(comment_year)
-            print(comments_and_visitors[key]['park_visitors'][comment_year].append(comment))
-            #comments_and_visitors[key][park_visitors][comment_year]['comments'].append(comment)
+            # print(comment_year)
+            # print([c for c in comments_and_visitors[key]['park_visitors']])
+            for visitor_year in comments_and_visitors[key]['park_visitors']:
+                if visitor_year == comment_year:
+                    visitor_year[comments].append(comment)
+            # comments_and_visitors[key]['park_visitors'][comment_year]['comments'].append(comment)
 
-# j = json.dumps(comments_and_visitors, indent=4)
-# #print(j)
-# with open('data/comments_and_visitors.json', 'w+') as f:
-#     print(j, file=f)    
+pprint(comments_and_visitors)
+
+j = json.dumps(comments_and_visitors, indent=4)
+#print(j)
+with open('data/comments_and_visitors.json', 'w+') as f:
+    print(j, file=f)    
